ca593c64d2b0ba6e40d814caea19cd32b62c5959
Takuya Kikuchi <takuyakikuch@gmail.com>
2023-07-15 17:21:17 +0900

    feat: add all_links field to the root query type

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,13 +1,9 @@
 module Types
   class QueryType < Types::BaseObject
-    # Add root-level fields here.
-    # They will be entry points for queries on your schema.
+    field :all_links, [LinkType], null: false
+  end
 
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
-    end
+  def all_links
+    Link.all
   end
 end
