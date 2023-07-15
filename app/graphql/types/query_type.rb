db5e4eee940ec9f395835aa9b312c3fdd19fca0e
Takuya Kikuchi <takuyakikuch@gmail.com>
2023-07-15 17:42:30 +0900

    fix: all_links

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,9 +1,9 @@
 module Types
   class QueryType < Types::BaseObject
     field :all_links, [LinkType], null: false
-  end
 
-  def all_links
-    Link.all
+    def all_links
+      Link.all
+    end
   end
 end
