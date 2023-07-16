df44b6abc2e9603289be4194d12739932556859b
Takuya Kikuchi <takuyakikuch@gmail.com>
2023-07-16 17:30:26 +0900

    feat: add user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_07_15_080904) do
+ActiveRecord::Schema.define(version: 2023_07_16_081406) do
 
   create_table "links", force: :cascade do |t|
     t.string "url"
     t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
