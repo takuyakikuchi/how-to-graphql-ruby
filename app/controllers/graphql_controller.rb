88288ee111f3576453aaa6d0ff7486b5c4c00318
Takuya Kikuchi <takuyakikuch@gmail.com>
2023-07-17 10:05:03 +0900

    fix: initialize MessageEncryptor

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -24,10 +24,10 @@ class GraphqlController < ApplicationController
   def current_user
     return unless session[:token]
 
-    crypt = ActiveSupport::MessageEncryptor.new(Rails.application.secrets.secret_key_base.byteslice(0..31))
+    crypt = ActiveSupport::MessageEncryptor.new(Rails.application.credentials.secret_key_base.byteslice(0..31))
     token = crypt.decrypt_and_verify session[:token]
     user_id = token.gsub('user-id:', '').to_i
-    User.find_by id: user_id
+    User.find user_id
   rescue ActiveSupport::MessageVerifier::InvalidSignature
     nil
   end
