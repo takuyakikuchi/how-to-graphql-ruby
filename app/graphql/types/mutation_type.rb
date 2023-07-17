2dd9756e731009f75e677f4460098e6da87bc805
Takuya Kikuchi <takuyakikuch@gmail.com>
2023-07-17 09:24:06 +0900

    feat: sing in mutation

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -2,5 +2,6 @@ module Types
   class MutationType < Types::BaseObject
     field :create_link, mutation: Mutations::CreateLink
     field :create_user, mutation: Mutations::CreateUser
+    field :signin_user, mutation: Mutations::SignInUser
   end
 end
