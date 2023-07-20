5e65414bd90d987fc9385a7464c84a5f57a38bbc
Takuya Kikuchi <takuyakikuch@gmail.com>
2023-07-20 15:05:33 +0900

    feat: add votes to user

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -3,5 +3,7 @@ module Types
     field :id, ID, null: false
     field :name, String, null: false
     field :email, String, null: false
+    field :votes, [Types::VoteType], null: false
+    field :links, [Types::LinkType], null: false
   end
 end
