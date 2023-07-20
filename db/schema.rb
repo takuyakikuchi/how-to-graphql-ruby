6f863eeb40c860437a2886390588ccfa07ec9426
Takuya Kikuchi <takuyakikuch@gmail.com>
2023-07-20 14:49:23 +0900

    feat: generate Vote model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_07_17_003836) do
+ActiveRecord::Schema.define(version: 2023_07_20_054755) do
 
   create_table "links", force: :cascade do |t|
     t.string "url"
@@ -29,5 +29,16 @@ ActiveRecord::Schema.define(version: 2023_07_17_003836) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "votes", force: :cascade do |t|
+    t.integer "link_id", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["link_id"], name: "index_votes_on_link_id"
+    t.index ["user_id"], name: "index_votes_on_user_id"
+  end
+
   add_foreign_key "links", "users"
+  add_foreign_key "votes", "links"
+  add_foreign_key "votes", "users"
 end
