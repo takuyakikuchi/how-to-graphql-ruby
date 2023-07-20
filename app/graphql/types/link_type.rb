5f60948e0a84f7836f63f442b6039833dd8f9ef1
Takuya Kikuchi <takuyakikuch@gmail.com>
2023-07-20 15:03:20 +0900

    feat: add votes to link

--- app/graphql/types/link_type.rb
+++ app/graphql/types/link_type.rb
@@ -4,5 +4,6 @@ module Types
     field :url, String, null: false
     field :description, String, null: false
     field :posted_by, UserType, null: true, method: :user
+    field :votes, [Types::VoteType], null: false
   end
 end
--- app/models/link.rb
+++ app/models/link.rb
@@ -1,3 +1,5 @@
 class Link < ApplicationRecord
   belongs_to :user
+
+  has_many :votes
 end
