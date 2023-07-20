f386a0f8443a50317419e5c2d55208f0ec7ed054
Takuya Kikuchi <takuyakikuch@gmail.com>
2023-07-20 14:53:20 +0900

    feat: define create vote resolver

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -3,5 +3,6 @@ module Types
     field :create_link, mutation: Mutations::CreateLink
     field :create_user, mutation: Mutations::CreateUser
     field :signin_user, mutation: Mutations::SignInUser
+    field :create_vote, mutation: Mutations::CreateVote
   end
 end
