62d60dd786fb5545341137c8080d95169d401102
Takuya Kikuchi <takuyakikuch@gmail.com>
2023-07-22 10:15:23 +0900

    feat: add validation to link and handles the error

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,5 +1,8 @@
 class Link < ApplicationRecord
   belongs_to :user
 
+  validates :url, presence: true, length: { minimum: 5 }
+  validates :description, presence: true, length: { minimum: 5 }
+
   has_many :votes
 end
