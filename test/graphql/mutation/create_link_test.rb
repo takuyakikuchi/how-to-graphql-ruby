e0471e58148a451ec1611ded2edaa29ce8d070c3
Takuya Kikuchi <takuyakikuch@gmail.com>
2023-07-22 10:59:07 +0900

    fix: create_link_test

--- test/graphql/mutation/create_link_test.rb
+++ test/graphql/mutation/create_link_test.rb
@@ -2,11 +2,22 @@ require 'test_helper'
 
 class Mutations::CreateLinkTest < ActiveSupport::TestCase
   def perform(user: nil, **args)
-    Mutations::CreateLink.new(object: nil, field: nil, context: {}).resolve(**args)
+    Mutations::CreateLink.new(object: nil, field: nil, context: { current_user: user}).resolve(**args)
   end
 
   test 'create a new link' do
+    user = Mutations::CreateUser.new(object: nil, field: nil, context: {}).resolve(
+      name: 'Test User',
+      auth_provider: {
+        credentials: {
+          email: 'email@example.com',
+          password: '[omitted]'
+        }
+      }
+    )
+
     link = perform(
+      user: user,
       url: 'http://example.com',
       description: 'description',
     )
